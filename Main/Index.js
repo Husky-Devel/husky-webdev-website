5336c7b5ffae6fb9277adb7219e287f06e5499cf
Peter <peter.gallwas@outlook.com>
2021-06-14 17:01:53 +1200

    3

--- Main/Index.js
+++ Main/Index.js
@@ -1,37 +1,8 @@
-/**
- * index.js
- */
-const express = require("express");
-const cors = require("cors");
-const authMiddleware = require("./auth-middleware");
-
-const app = express();
-app.use(cors());
-
-const books = [
-    {
-        id: 1,
-        name: "Harry Potter",
-        image:
-            "https://pmpub-catalogue.s3-eu-west-1.amazonaws.com/covers/web/9781781100240.jpg",
-    },
-    {
-        id: 2,
-        name: "Clean Code",
-        image:
-            "https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L._SX374_BO1,204,203,200_.jpg",
-    },
-    {
-        id: 3,
-        name: "Javascript: The good parts",
-        image: "https://images-na.ssl-images-amazon.com/images/I/81kqrwS1nNL.jpg",
-    },
-];
-
-app.use("/", authMiddleware);
-
-app.get("/books", (request, response) => {
-    return response.send({ books });
-});
-
-app.listen(4000, () => console.log("The server is running at PORT 4000"));
+const getQueryParams = ( params, url ) => {
+    let href = url;
+    // this is an expression to get query strings
+    let regexp = new RegExp( '[?&]' + params + '=([^&#]*)', 'i' );
+    let qString = regexp.exec(href);
+    return qString ? qString[1] : null;
+};
+console.log(getQueryParams('data','http://example.com?example=something&data=32'));
